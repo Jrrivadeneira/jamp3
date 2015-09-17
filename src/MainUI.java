0a9b67650b1fafb90f1404042eb46411f471c855
jack <jrrivadeneira@wpi.edu>
2015-09-17 13:56:12 -0400

    Created config directory and MainUI.conf

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,10 +1,12 @@
 // Written by Jack Rivadeneira on 20150917122321
 import javax.swing.*;
 public class MainUI extends JFrame{
+	// Main UI class.
 	public MainUI(){
+		System.out.println("Building Main UI");
+		// TODO: These values should be read from a .conf
 		this.setTitle("JAMP3 V0.01");
 		this.setSize(500,500);
-
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setVisible(true);
 		System.out.println("Done.");
